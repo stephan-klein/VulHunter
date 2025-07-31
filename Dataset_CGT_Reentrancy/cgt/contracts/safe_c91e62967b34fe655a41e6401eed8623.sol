25a857a369bfd7e8e366ae6018669cde75565050
Stephan Klein <sklein.at@gmail.com>
2025-07-31 16:26:31 +0000

    Update dataset

--- Dataset_CGT_Reentrancy/cgt/contracts/safe_c91e62967b34fe655a41e6401eed8623.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.0;
-contract HelloWorld {  
-    event log_string(bytes32 log); // Event
-
-    function () { // Fallback Function
-        log_string("Hello World!");
-    }
-}
